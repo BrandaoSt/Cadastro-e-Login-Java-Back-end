cb7d06cc7c21c20fdf4a1605476724a22b738a18
Patricia Brandão <patriciabrandao7@gmail.com>
2022-03-21 09:20:37 -0300

    Apagando chaves de segurança expostas

--- src/main/java/br/com/eurotrip/services/AbstractEmailService.java
+++ src/main/java/br/com/eurotrip/services/AbstractEmailService.java
@@ -22,11 +22,15 @@ public abstract class AbstractEmailService implements EmailService {
 		SimpleMailMessage sm = new SimpleMailMessage();
 		sm.setTo(usuario.getEmail());
 		sm.setFrom(sender);
-		sm.setSubject("Recuperação de Senha COLORE");
+		sm.setSubject("Recuperação de Senha EUROTRIP");
 		sm.setSentDate(new Date(System.currentTimeMillis()));
-		sm.setText("Por favor, use a seguinte senha para fazer login em sua conta COLORE.<br> Nova senha: " + newPass + "<br> Se você não solicitou uma alteração de senha, sinta-se à vontade para ignorar esta mensagem.\r\n"
+		sm.setText("Por favor, use a seguinte senha para fazer login em sua conta Eurotrip."
+				+ "Nova senha: " + newPass + "  "
+				+ "Se você não solicitou uma alteração de senha, sinta-se à vontade para ignorar esta mensagem.\r\n"
 				+ "\r\n"
-				+ "Se você tiver quaisquer comentários ou perguntas, não hesite em nos contatar através da nossa equipe de suporte no portal de Contato Colore ou sinta-se à vontade para responder a este e-mail. Ele foi enviado de um endereço de e-mail monitorado e gostaríamos de saber de você.<br> A Colore agradece seu contato. Até mais!");
+				+ "Para dúvidas, sugestões ou reclamações não hesite em nos contatar através da nossa equipe de suporte no portal de Contato Eurotrip ou sinta-se à vontade para responder a este e-mail. "
+				+ "Ele foi enviado de um endereço de e-mail monitorado e gostaríamos de receber sua mensagem. "
+				+ "A Eurotrip agradece o seu contato. Até mais!");
 		return sm;
 	}
 }
--- src/test/java/br/com/eurotrip/UserEurotripApplicationTests.java
+++ src/test/java/br/com/eurotrip/UserEurotripApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.colore;
+package br.com.eurotrip;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserColoreApplicationTests {
+class UserEurotripApplicationTests {
 
 	@Test
 	void contextLoads() {
